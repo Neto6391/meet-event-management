9b81bb45c697fd49caf1823b4e3c9f12456dcd31
Neto6391 <neto6391@gmail.com>
2024-01-09 15:29:08 -0300

    feat(create put user route)

--- start/routes.ts
+++ start/routes.ts
@@ -3,5 +3,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.group(() => {
     Route.post('/', 'UsersController.store')
+    Route.put('/:id', 'UsersController.update')
   }).prefix('/users')
 }).prefix('/api/v1')
